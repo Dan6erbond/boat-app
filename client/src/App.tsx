41e4b8127d03306bf500b75395e6b0069e2f9682
RaviAnand Mohabir <moravrav@gmail.com>
2022-01-18 18:53:24 +0100

    ✨ Implement Sign Out Functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,24 @@ import {
   Heading,
   HStack,
   IconButton,
-  useColorMode,
+  useColorMode
 } from "@chakra-ui/react";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { RequireAuth } from "./components/RequireAuth";
+import { RootState } from "./lib/store";
+import { signOut } from "./lib/store/userSlice";
 import { BoatsPage } from "./pages/boats";
 import { BoatPage } from "./pages/boats/[id]";
 import { EditBoatPage } from "./pages/boats/[id]/edit";
 import { LoginPage } from "./pages/login";
 
 function App() {
+  const { user } = useSelector((state: RootState) => state.user);
   const { colorMode, toggleColorMode } = useColorMode();
+  const dispatch = useDispatch();
 
   return (
     <Router>
@@ -34,7 +39,7 @@ function App() {
           borderBottomWidth={1}
           justify="space-between"
         >
-          <Heading as={Link} to="/">
+          <Heading as={Link} to="/" size="lg">
             Boat App
           </Heading>
           <HStack spacing={2}>
@@ -51,7 +56,13 @@ function App() {
                 onClick={toggleColorMode}
               />
             )}
-            <IconButton aria-label="Sign out" icon={<LockIcon />} />
+            {user && (
+              <IconButton
+                aria-label="Sign out"
+                icon={<LockIcon />}
+                onClick={() => dispatch(signOut())}
+              />
+            )}
           </HStack>
         </Flex>
         <Box p={4}>
